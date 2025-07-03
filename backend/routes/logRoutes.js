24f411445cd6665bdacfcea99efd334ce119498a
MedaSivaManoj <sivamanoj1406@gmail.com>
2025-07-03 20:41:35 +0530

    delete old project files

--- backend/routes/logRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getRecentLogs } = require("../controllers/logController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.get("/", protect, getRecentLogs);
-
-module.exports = router;
